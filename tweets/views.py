f0f3c163e5d25c82c0430edc43fc25c54c66d439
Kevin Liu <kevin07liu@gmail.com>
2020-05-11 18:36:22 +0800

    [16/110] Dynamic Load Tweets via JavaScript

--- tweets/views.py
+++ tweets/views.py
@@ -13,7 +13,7 @@ def home_view(request, *args, **kwargs):
 def tweet_list_view(request, *args, **kwargs):
     qs = Tweet.objects.all()
     tweets_list = [{"id": x.id, "content": x.content} for x in qs]
-    data = {"response": tweets_list}
+    data = {"isUser": False, "response": tweets_list}
     return JsonResponse(data)
 
 
