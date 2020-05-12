f953012d8adbed05064c9e89ac9e1c50996caece
Kevin Liu <kevin07liu@gmail.com>
2020-05-13 07:59:30 +0800

    [33/110] Users & Tweets

--- tweets/models.py
+++ tweets/models.py
@@ -1,11 +1,19 @@
 from django.db import models
+from django.conf import settings
 
 import random
 
 # Create your models here.
 
+User = settings.AUTH_USER_MODEL
+
 
 class Tweet(models.Model):
+    # Maps to SQL data
+    # id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE
+    )  # many users can have many tweets
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to="images/", blank=True, null=True)
 
