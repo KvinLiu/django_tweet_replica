a7d4bab59080ce28d618bc663faf3f92f0e7a759
Kevin Liu <kevin07liu@gmail.com>
2020-05-12 22:06:04 +0800

    [32/110] Rendering the Error Message via Vanilla JavaScript

--- tweets/views.py
+++ tweets/views.py
@@ -33,7 +33,7 @@ def tweet_create_view(request, *args, **kwargs):
 
     if form.errors:
         if request.is_ajax():
-            return JsonResponse(form.errors, status=300)
+            return JsonResponse(form.errors, status=400)
     return render(request, "components/form.html", context={"form": form})
 
 
