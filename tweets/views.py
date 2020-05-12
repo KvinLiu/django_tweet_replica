3b6b859fae525c87588b200893be4ff664d83b4d
Kevin Liu <kevin07liu@gmail.com>
2020-05-12 21:34:31 +0800

    [31/110] Handling Form Errors

--- tweets/views.py
+++ tweets/views.py
@@ -30,6 +30,10 @@ def tweet_create_view(request, *args, **kwargs):
             return redirect(next_url)
 
         form = TweetForm()
+
+    if form.errors:
+        if request.is_ajax():
+            return JsonResponse(form.errors, status=300)
     return render(request, "components/form.html", context={"form": form})
 
 
