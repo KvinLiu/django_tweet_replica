79177a5256aacf2c4629d88d63e441d4ba1bee3a
Kevin Liu <kevin07liu@gmail.com>
2020-05-13 21:37:17 +0800

    [41/110] Delete Tweet API View

--- tweets/urls.py
+++ tweets/urls.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 from django.urls import path
-from .views import home_view, tweet_detail_view, tweet_list_view, tweet_create_view
+from .views import (
+    home_view,
+    tweet_detail_view,
+    tweet_list_view,
+    tweet_create_view,
+    tweet_delete_view,
+)
 
 app_name = "tweets"
 
@@ -9,5 +15,6 @@ urlpatterns = [
     path("", home_view, name="homepage"),
     path("tweets", tweet_list_view, name="tweets"),
     path("tweets/<int:tweet_id>", tweet_detail_view, name="detail"),
+    path("api/tweets/<int:tweet_id>/delete", tweet_delete_view, name="delete"),
     path("create-tweet", tweet_create_view, name="create_tweet"),
 ]
--- tweets/views.py
+++ tweets/views.py
@@ -53,6 +53,20 @@ def tweet_detail_view(request, tweet_id):
     return Response(serializer.data)
 
 
+@api_view(["DELETE", "POST"])
+@permission_classes([IsAuthenticated])
+def tweet_delete_view(request, tweet_id):
+    qs = Tweet.objects.filter(id=tweet_id)
+    if not qs.exists():
+        return Response({}, status=404)
+    qs = qs.filter(user=request.user)
+    if not qs.exists():
+        return Response({"message": "You can not delete this tweet"}, status=401)
+    obj = qs.first()
+    obj.delete()
+    return Response({"message": "Tweet Removed"}, status=200)
+
+
 def tweet_create_view_pure_django(request, *args, **kwargs):
     user = request.user
     if not request.user.is_authenticated:
