0a4e7a4704eaf34a32a37e2ab9cce0a88f448626
Kevin Liu <kevin07liu@gmail.com>
2020-05-16 14:35:00 +0800

    [53/110] Understanding Functional Components

--- tweetme2-web/src/App.js
+++ tweetme2-web/src/App.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [tweets, setTweets] = useState([]);
+  useEffect(() => {
+    const tweetItems = [{ content: 123 }, { content: "Hello World" }];
+    setTweets(tweetItems);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          {tweets.map((tweet, index) => {
+            return <li>{tweet.content}</li>;
+          })}
         </p>
         <a
           className="App-link"
