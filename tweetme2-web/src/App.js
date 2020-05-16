f93e0a6a870bbfeecf5d7bf04d6731c621dea64d
Kevin Liu <kevin07liu@gmail.com>
2020-05-16 18:43:07 +0800

    [57/110] React.js Action Btn

--- tweetme2-web/src/App.js
+++ tweetme2-web/src/App.js
@@ -19,6 +19,16 @@ function loadTweets(callback) {
   xhr.send();
 }
 
+function ActionBtn(props) {
+  const { tweet, action } = props;
+  const className = props.className
+    ? props.className
+    : "btn btn-primary btn-small";
+  return action.type === "like" ? (
+    <button className={className}>{tweet.likes} Likes</button>
+  ) : null;
+}
+
 function Tweet(props) {
   const { tweet } = props;
   const className = props.className
@@ -29,6 +39,9 @@ function Tweet(props) {
       <p>
         {tweet.id} - {tweet.content}
       </p>
+      <div className="btn btn-group">
+        <ActionBtn tweet={tweet} action={{ type: "like" }} />
+      </div>
     </div>
   );
 }
