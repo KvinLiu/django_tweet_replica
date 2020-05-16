54950ad6835901484064cfab4969919f61895c1f
Kevin Liu <kevin07liu@gmail.com>
2020-05-16 15:59:03 +0800

    [55/110] Handling CORS and Invalid HOST_HEADER in Django

--- tweetme2-web/src/App.js
+++ tweetme2-web/src/App.js
@@ -2,6 +2,23 @@ import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+function loadTweets(callback) {
+  const xhr = new XMLHttpRequest();
+  const method = "GET";
+  const url = "http://localhost:8000/api/tweets/";
+  const responseType = "json";
+  xhr.responseType = responseType;
+  xhr.open(method, url);
+  xhr.onload = function () {
+    callback(xhr.response, xhr.status);
+  };
+  xhr.onerror = function (e) {
+    console.log(e);
+    callback({ message: "The request was on error" }, 400);
+  };
+  xhr.send();
+}
+
 function App() {
   const [tweets, setTweets] = useState([]);
   useEffect(() => {
@@ -41,20 +58,4 @@ function App() {
   );
 }
 
-function loadTweets(callback) {
-  const xhr = new XMLHttpRequest();
-  const method = "GET";
-  const url = "localhost:3000/api/tweets";
-  const responseType = "json";
-  xhr.responseType = responseType;
-  xhr.open(method, url);
-  xhr.onload = function () {
-    callback(xhr.reponse, xhr.status);
-  };
-  xhr.onerror = function (e) {
-    console.log(e);
-    callback({ message: "The request was on error" }, 400);
-  };
-  xhr.send();
-}
 export default App;
