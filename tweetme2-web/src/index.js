df357ebff73764df55a56e309dfaa58612bdf938
Kevin Liu <kevin07liu@gmail.com>
2020-05-17 15:24:02 +0800

    [63/110] Adjust the React Render Process

--- tweetme2-web/src/index.js
+++ tweetme2-web/src/index.js
@@ -1,16 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { TweetsComponent } from "./tweets";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
+const appEl = document.getElementById("root");
+if (appEl) {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    appEl
+  );
+}
+const tweetsEl = document.getElementById("tweetme-2");
+if (tweetsEl) {
+  ReactDOM.render(<TweetsComponent />, tweetsEl);
+}
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
