68c952f4ac787cf900fbc6bbd16eac6b48c2938c
Kevin Liu <kevin07liu@gmail.com>
2020-05-21 17:19:08 +0800

    [86/110] Handling Profile Does Not Exist

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,4 +3,4 @@ from django.urls import path
 from .views import profile_detail_view
 
 app_name = "profiles"
-urlpatterns = [path("<str:username>", profile_detail_view)]
+urlpatterns = [path("<str:username>", profile_detail_view, name="profile_detail")]
--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,13 @@
+from django.http import Http404
 from django.shortcuts import render
+
+from .models import Profile
 
 # Create your views here.
 def profile_detail_view(request, username):
+    qs = Profile.objects.filter(user__username=username)
+    if not qs.exists():
+        raise Http404
+    profile_obj = qs.first()
+    context = {"username": username, "profile": profile_obj}
     return render(request, "profiles/detail.html", {"username": username})
