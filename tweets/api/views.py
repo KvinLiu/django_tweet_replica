e10562be19e9e580505d255ec103a1e72391bc06
Kevin Liu <kevin07liu@gmail.com>
2020-05-22 20:36:09 +0800

    [95/110] More Efficient List Views with Pagination

--- tweets/api/views.py
+++ tweets/api/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render, redirect
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
 
 from ..forms import TweetForm
 from ..models import Tweet
@@ -29,13 +30,21 @@ def tweet_create_view(request, *args, **kwargs):
     return Response({}, status=400)
 
 
+def get_paginated_queryset_response(qs, request):
+    paginator = PageNumberPagination()
+    paginator.page_size = 20
+    paginated_qs = paginator.paginate_queryset(qs, request)
+    serializer = TweetSerializer(paginated_qs, many=True)
+    return paginator.get_paginated_response(serializer.data)
+
+
 @api_view(["GET"])
 @permission_classes([IsAuthenticated])
 def tweet_feed_view(request, *args, **kwargs):
     user = request.user
     qs = Tweet.objects.feed(user)
-    serializer = TweetSerializer(qs, many=True)
-    return Response(serializer.data, status=200)
+    # return Response(serializer.data, status=200)
+    return get_paginated_queryset_response(qs, request)
 
 
 @api_view(["GET"])
@@ -44,8 +53,7 @@ def tweet_list_view(request, *args, **kwargs):
     username = request.GET.get("username")  # ?username=justin
     if username != None:
         qs = qs.by_username(username)
-    serializer = TweetSerializer(qs, many=True)
-    return Response(serializer.data, status=200)
+    return get_paginated_queryset_response(qs, request)
 
 
 @api_view(["GET"])
