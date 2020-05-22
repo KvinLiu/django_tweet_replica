2088831bc83308a71b465c40886e7dd911136273
Kevin Liu <kevin07liu@gmail.com>
2020-05-22 20:20:25 +0800

    [94/110] More Efficient Backend Lookups and Custom Model Managers

--- tweets/api/views.py
+++ tweets/api/views.py
@@ -33,12 +33,7 @@ def tweet_create_view(request, *args, **kwargs):
 @permission_classes([IsAuthenticated])
 def tweet_feed_view(request, *args, **kwargs):
     user = request.user
-    profiles = user.following.all()
-    followed_users_ids = []
-    if profiles.exists():
-        followed_users_ids = [x.user.id for x in profiles]
-    followed_users_ids.append(user.id)
-    qs = Tweet.objects.filter(user__id__in=followed_users_ids).order_by("-timestamp")
+    qs = Tweet.objects.feed(user)
     serializer = TweetSerializer(qs, many=True)
     return Response(serializer.data, status=200)
 
@@ -48,7 +43,7 @@ def tweet_list_view(request, *args, **kwargs):
     qs = Tweet.objects.all()
     username = request.GET.get("username")  # ?username=justin
     if username != None:
-        qs = qs.filter(user__username__iexact=username)
+        qs = qs.by_username(username)
     serializer = TweetSerializer(qs, many=True)
     return Response(serializer.data, status=200)
 
--- tweets/models.py
+++ tweets/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import Q
 from django.conf import settings
 
 import random
@@ -14,6 +15,30 @@ class TweetLike(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
 
 
+class TweetQuerySet(models.QuerySet):
+    def by_username(self, username):
+        return self.filter(user__username__iexact=username)
+
+    def feed(self, user):
+        profiles_exist = user.following.exists()
+        followed_users_id = []
+        if profiles_exist:
+            followed_users_id = user.following.values_list("user__id", flat=True)
+        return (
+            self.filter(Q(user__id__in=followed_users_id) | Q(user=user))
+            .distinct()
+            .order_by("-timestamp")
+        )
+
+
+class TweetManager(models.Manager):
+    def get_queryset(self, *args, **kwargs):
+        return TweetQuerySet(self.model, using=self._db)
+
+    def feed(self, user):
+        return self.get_queryset().feed(user)
+
+
 class Tweet(models.Model):
     # Maps to SQL data
     # id = models.AutoField(primary_key=True)
@@ -27,6 +52,8 @@ class Tweet(models.Model):
         User, related_name="tweet_user", blank=True, through=TweetLike
     )
     timestamp = models.DateTimeField(auto_now_add=True)
+
+    objects = TweetManager()
 
     class Meta:
         ordering = ["-id"]
