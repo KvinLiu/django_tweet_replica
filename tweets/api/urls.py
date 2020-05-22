309195b99f5d9fb519c767c569ce4f910cc1d22f
Kevin Liu <kevin07liu@gmail.com>
2020-05-22 19:02:48 +0800

    [93/110] User Feed Including Following

--- tweets/api/urls.py
+++ tweets/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import (
     tweet_detail_view,
+    tweet_feed_view,
     tweet_list_view,
     tweet_create_view,
     tweet_delete_view,
@@ -15,6 +16,7 @@ app_name = "tweets_api"
 
 urlpatterns = [
     path("", tweet_list_view, name="tweets"),
+    path("feed/", tweet_feed_view, name="feeds"),
     path("action/", tweet_action_view, name="action"),
     path("create/", tweet_create_view, name="create"),
     path("<int:tweet_id>/", tweet_detail_view, name="detail"),
--- tweets/api/views.py
+++ tweets/api/views.py
@@ -30,13 +30,27 @@ def tweet_create_view(request, *args, **kwargs):
 
 
 @api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def tweet_feed_view(request, *args, **kwargs):
+    user = request.user
+    profiles = user.following.all()
+    followed_users_ids = []
+    if profiles.exists():
+        followed_users_ids = [x.user.id for x in profiles]
+    followed_users_ids.append(user.id)
+    qs = Tweet.objects.filter(user__id__in=followed_users_ids).order_by("-timestamp")
+    serializer = TweetSerializer(qs, many=True)
+    return Response(serializer.data, status=200)
+
+
+@api_view(["GET"])
 def tweet_list_view(request, *args, **kwargs):
     qs = Tweet.objects.all()
     username = request.GET.get("username")  # ?username=justin
     if username != None:
         qs = qs.filter(user__username__iexact=username)
     serializer = TweetSerializer(qs, many=True)
-    return Response(serializer.data)
+    return Response(serializer.data, status=200)
 
 
 @api_view(["GET"])
