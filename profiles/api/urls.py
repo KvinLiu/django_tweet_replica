de8caff74cb0fbdb6b7e7533235e3141d728ac81
Kevin Liu <kevin07liu@gmail.com>
2020-05-24 20:01:02 +0800

    [103/110] User Profile API Detail

--- profiles/api/urls.py
+++ profiles/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import user_follow_view
+from .views import user_follow_view, profile_detail_api_view
 
 """
 CLIENT
@@ -7,4 +7,7 @@ Base ENDPOINT /api/profiles/
 """
 app_name = "profile_api"
 
-urlpatterns = [path("<str:username>/follow", user_follow_view, name="follow")]
+urlpatterns = [
+    path("<str:username>/", profile_detail_api_view, name="detail"),
+    path("<str:username>/follow", user_follow_view, name="follow"),
+]
--- profiles/api/views.py
+++ profiles/api/views.py
@@ -9,9 +9,20 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from ..models import Profile
+from ..serializers import PublicProfileSerializer
 
 ALLOWED_HOSTS = settings.ALLOWED_HOSTS
 User = get_user_model()
+
+
+@api_view(["GET"])
+def profile_detail_api_view(request, username):
+    qs = Profile.objects.filter(user__username=username)
+    if not qs.exists():
+        return Response({"detail": "User not found"}, status=404)
+    profile_obj = qs.first()
+    data = PublicProfileSerializer(instance=profile_obj)
+    return Response(data.data, status=200)
 
 
 @api_view(["GET"])
