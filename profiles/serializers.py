42da91aa41d8758d5f1484163c6d63de9ba77e03
Kevin Liu <kevin07liu@gmail.com>
2020-05-24 21:08:59 +0800

    [104/110] Passing the Request to Serializers

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -7,6 +7,7 @@ class PublicProfileSerializer(serializers.ModelSerializer):
     first_name = serializers.SerializerMethodField(read_only=True)
     last_name = serializers.SerializerMethodField(read_only=True)
     username = serializers.SerializerMethodField(read_only=True)
+    is_following = serializers.SerializerMethodField(read_only=True)
     followers_count = serializers.SerializerMethodField(read_only=True)
     following_count = serializers.SerializerMethodField(read_only=True)
 
@@ -20,8 +21,18 @@ class PublicProfileSerializer(serializers.ModelSerializer):
             "location",
             "followers_count",
             "following_count",
+            "is_following",
             "username",
         ]
+
+    def get_is_following(self, obj):
+        context = self.context
+        request = context.get("request")
+        is_following = False
+        if request:
+            user = request.user
+            is_following = user in obj.followers.all()
+        return is_following
 
     def get_first_name(self, obj):
         return obj.user.first_name
